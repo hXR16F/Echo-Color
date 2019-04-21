e5672141831b86a01cceba479e3cd6be838146c5
hXR16F <hxr16f.ar@gmail.com>
2019-04-21 17:02:15 +0200

    Add files via upload

--- src/example.bat
+++ src/example.bat
@@ -1,7 +1,9 @@
 @echo off & mode 40,40 & cls
-for /f "tokens=1,2*" %%i in (echocl.lst) do call echocl.bat "TEST TEST TEST TEST" %%i %%j
+for /f "tokens=1,2,3*" %%i in (echocl.lst) do call echocl.bat "TEST TEST TEST TEST" %%i %%j %%k
 pause & cls
-for /f "tokens=1,2*" %%i in (echocl.lst) do call echocl.bat "%date%, %time:~0,8%" %%i %%j
+for /f "tokens=1,2,3*" %%i in (echocl.lst) do call echocl.bat "%date%, %time:~0,8%" %%i %%j %%k
 pause & cls
-for /f "tokens=1,2*" %%i in (echocl.lst) do call echocl.bat "                         " %%i %%j
+for /f "tokens=1,2,3*" %%i in (echocl.lst) do call echocl.bat "                                        " %%i %%j %%k
+pause & cls
+for /l %%a in (1,1,8) do for /f "tokens=1,2,3*" %%i in (echocl.lst) do call echocl.bat " %%j,%%k " n %%j %%k
 pause
