4e7eb8e4321a11ee94690c557f219bc1ca9854e6
hXR16F <hxr16f.ar@gmail.com>
2019-06-24 21:24:20 +0200

    Add files via upload

---
+++ src/example1.bat
@@ -0,0 +1,9 @@
+@echo off & mode 40,40 & cls
+for /f "tokens=1,2,3*" %%i in (example1.lst) do call echocl.bat "TEST TEST TEST TEST" %%i %%j %%k
+pause & cls
+for /f "tokens=1,2,3*" %%i in (example1.lst) do call echocl.bat "%date%, %time:~0,8%" %%i %%j %%k
+pause & cls
+for /f "tokens=1,2,3*" %%i in (example1.lst) do call echocl.bat "                                        " %%i %%j %%k
+pause & cls
+for /l %%a in (1,1,8) do for /f "tokens=1,2,3*" %%i in (example1.lst) do call echocl.bat " %%j,%%k " n %%j %%k
+pause
---
+++ src/example2.bat
@@ -0,0 +1,42 @@
+@echo off
+color 07
+
+call echocl.bat "Hello World!" y f 0
+call echocl.bat "Hello World!" y a 0
+call echocl.bat "Hello World!" y b 0
+call echocl.bat "Hello World!" y c 0
+call echocl.bat "Hello World!" y d 0
+call echocl.bat "Hello World!" y e 0
+
+echo.
+
+call echocl.bat "Hello World! " n f 0
+call echocl.bat "Hello World! " n a 0
+call echocl.bat "Hello World! " n b 0
+call echocl.bat "Hello World! " n c 0
+call echocl.bat "Hello World! " n d 0
+call echocl.bat "Hello World! " n e 0
+
+echo.
+echo.
+
+call echocl.bat "Hello World!" y 0 f
+call echocl.bat "Hello World!" y 0 a
+call echocl.bat "Hello World!" y 0 b
+call echocl.bat "Hello World!" y 0 c
+call echocl.bat "Hello World!" y 0 d
+call echocl.bat "Hello World!" y 0 e
+
+echo.
+
+call echocl.bat "Hello World!" n 0 f
+call echocl.bat "Hello World!" n 0 a
+call echocl.bat "Hello World!" n 0 b
+call echocl.bat "Hello World!" n 0 c
+call echocl.bat "Hello World!" n 0 d
+call echocl.bat "Hello World!" n 0 e
+
+echo.
+echo.
+
+pause
